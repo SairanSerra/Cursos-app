2d7eb8cad13cec6cde934e3abaa911c3c93fccf3
sairan_serra <sserra@somoslogin.com.br>
2022-06-14 22:54:00 -0300

    tela login pronta

--- components/login.js
+++ components/login.js
@@ -1,59 +1,80 @@
 import React from 'react';
-import {StyleSheet, Image, TextInput, Button, KeyboardAvoidingView, ScrollView} from 'react-native';
-import { Header } from 'react-navigation-stack';
+import { StyleSheet, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView, Text } from 'react-native';
 import xildemy from '../assets/xildemy.png';
 
-function logar(){
+function logar() {
     alert('Logado com sucesso!')
 }
 
-export default function login(){
-    return(
-    <ScrollView>
-        <KeyboardAvoidingView  style={estilos.container} behavior='padding'>      
-            <Image source={xildemy} style={estilos.img}/>                
-            <TextInput
-            placeholder='Email'
-            style={estilos.input}
-            />           
+export default function login() {
+    return (
+        <ScrollView style={estilos.container}> 
+                <KeyboardAvoidingView style={estilos.keybord} behavior='padding'>
+                    <Image source={xildemy} style={estilos.img} />
 
-            <TextInput 
-            placeholder='Senha'
-            keyboardType='number'
-            style={estilos.input}
-            />        
-            
-            <Button title='Login' onPress={logar} />
-        </KeyboardAvoidingView> 
-    </ScrollView>
+                    <TextInput
+                        placeholder='Email'
+                        placeholderTextColor='#093366'
+                        style={estilos.input}
+                    />
+
+                    <TextInput
+                        placeholder='Senha'
+                        placeholderTextColor='#093366'
+                        keyboardType='numeric'
+                        style={estilos.input}
+                    />
+
+                    <TouchableOpacity title='Login'
+                        onPress={logar}
+                        style={estilos.botao}>
+                        <Text style={estilos.texto}>Login</Text>
+                    </TouchableOpacity>
+
+                </KeyboardAvoidingView>
+        </ScrollView>
     )
-     
+
 }
 
 
 const estilos = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#093366',
-        alignItems:'center',
+    container: {
+        flex: 1,
+        backgroundColor: '#093366',
     },
-    text:{
-        fontSize:40,
-        color:'#FFFFFF'
+    keybord: {
+        alignItems: 'center',
     },
-    img:{
-        marginBottom:100
+    text: {
+        fontSize: 40,
+        color: '#FFFFFF'
     },
-   input:{
-    backgroundColor:'#FFFFFF',
-    borderColor: "#FFFFFF",
-    width:350,
-    marginTop:10
-       
-   },
-   botao:{
-    width:100,
-    marginTop:10,
-    backgroundColor:'#1ABC9C'
-   }
+    img: {
+        marginBottom: 100,
+        marginTop: 30
+    },
+    input: {
+        backgroundColor: '#FFFFFF',
+        borderColor: '#FFFFFF',
+        color: '#093366',
+        width: 350,
+        marginTop: 10
+
+    },
+    texto: {
+        fontSize: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginTop: 2
+    },
+    botao: {
+        width: 100,
+        height: 40,
+        borderRadius: 5,
+        marginTop: 40,
+        backgroundColor: '#1ABC9C'
+    }
 })
